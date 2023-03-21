e8304fabb62618c7163e1b0224f746e5397c2ad9
emil mustonen <emppu.mustonen1@gmail.com>
2023-03-21 18:13:04 +0200

    Visual changes and mobile readability

--- portfolio/src/pages/About.js
+++ portfolio/src/pages/About.js
@@ -7,14 +7,14 @@ import Skills from '../components/skills'
 const About = () => {
   return(
     <div className='aboutPage'>
-        <h3 style={{color: 'white', fontSize: '70px', marginBottom: '50px'}}>ABOUT</h3>
+        <h2>ABOUT</h2>
         <div className='aboutContainer'>
-        <img className='picture' src ={me} alt='blankimg' />
-        <div className='pContainer'>
-          <p style={{color: 'white', fontSize: '30px', marginBottom: '5px'}}>Who am I?</p>
-          <p style={{color: 'white', fontSize: '20px', marginBottom: '25px'}}>I am <span style={{color: '#04AA6D'}}>Emil Mustonen</span> from Finland and I study ICT at Turku University of Applied Sciences, My goal is to become a full-stack developer. I work mostly with React and JS </p>
-          <Skills></Skills>
-        </div>
+          <img className='picture' src ={me} alt='me' />
+          <div>
+            <p style={{color: 'white', fontSize: '30px', marginBottom: '5px'}}>Who am I?</p>
+            <p style={{color: 'white', fontSize: '20px', marginBottom: '25px'}}>I am <span style={{color: '#04AA6D'}}>Emil Mustonen</span> from Finland and I study Information and Communications technology (engineering) at Turku University of Applied Sciences, My goal is to become a full-stack developer. I have mainly worked with React and JS. I have a passion for learning new things related to technology and this website is a good example of that </p>
+            <Skills></Skills>
+          </div>
         </div>
       </div>
   )
--- portfolio/src/pages/Contact.js
+++ portfolio/src/pages/Contact.js
@@ -7,12 +7,12 @@ import ContactForm from '../components/contactform';
 const Contact = () => {
   return (
     <div className='contactPage'>
-      <h3 style={{color: 'white', fontSize: '70px'}}>CONTACT</h3>
-      <ContactForm />
       <div className='contactContainer'>
+        <h2>CONTACT</h2>
+        <ContactForm />
+      </div>
+      <div className='contactContainer2'>
         <Button styling='ghButton' link='https://github.com/Arch4ic'></Button>
-        <Button styling='fbButton' link='https://www.facebook.com/emil.mustonen/'></Button>
-        <Button styling='gmButton' link='mailto:emil.mustonen@gmail.com'></Button>
         <Button styling='linkButton'link='https://www.linkedin.com/in/EmilMustonenFI/'></Button>
         <Button styling='shareButton' link='https://linktr.ee/emilmustonen'></Button>
       </div>
