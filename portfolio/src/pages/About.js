0af2bdb6385857050b5cb02f5a6c523c64265936
emil mustonen <emppu.mustonen1@gmail.com>
2023-03-21 18:39:21 +0200

    uibuild

--- portfolio/src/pages/About.js
+++ portfolio/src/pages/About.js
@@ -11,8 +11,8 @@ const About = () => {
         <div className='aboutContainer'>
           <img className='picture' src ={me} alt='me' />
           <div>
-            <p style={{color: 'white', fontSize: '30px', marginBottom: '5px'}}>Who am I?</p>
-            <p style={{color: 'white', fontSize: '20px', marginBottom: '25px'}}>I am <span style={{color: '#04AA6D'}}>Emil Mustonen</span> from Finland and I study Information and Communications technology (engineering) at Turku University of Applied Sciences, My goal is to become a full-stack developer. I have mainly worked with React and JS. I have a passion for learning new things related to technology and this website is a good example of that </p>
+            <p className='pabout'>Who am I?</p>
+            <p className='pabout2'>I am <span style={{color: '#04AA6D'}}>Emil Mustonen</span> from Finland and I study Information and Communications technology (engineering) at Turku University of Applied Sciences, My goal is to become a full-stack developer. I have mainly worked with React and JS. I have a passion for learning new things related to technology and this website is a good example of that </p>
             <Skills></Skills>
           </div>
         </div>
